708746c85a781a3f156c4b4f21b70be471b18df7
Eric Tsiliacos <erictsiliacos@gmail.com>
2022-10-05 14:50:35 -0400

    Add ability to notify when a truck is bought

---
+++ src/main/java/com/wehaul/fleet/messaging/NotificationService.java
@@ -0,0 +1,26 @@
+package com.wehaul.fleet.messaging;
+
+import com.wehaul.fleet.domain.DomainEventPublisher;
+import com.wehaul.fleet.domain.Truck;
+import com.wehaul.fleet.domain.TruckAvailabilityChanged;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
+
+@Service
+@Slf4j
+public record NotificationService(StreamBridge streamBridge) implements DomainEventPublisher {
+
+    @Override
+    public void publish(TruckAvailabilityChanged truckAvailabilityChanged) {
+        streamBridge.send("availability", truckAvailabilityChanged);
+    }
+
+    @Bean
+    public Consumer<TruckAvailabilityChanged> availabilityReceived() {
+        return truckAvailabilityChanged -> log.info("Received: {}", truckAvailabilityChanged);
+    }
+}
