70c5214286af48456d70754e0ddbad6c80b8bb7f
Cora Iberkleid <ciberkleid@gmail.com>
2022-10-06 17:45:26 -0400

    add httptrace actuator endpoint

--- src/main/java/com/wehaul/fleet/Application.java
+++ src/main/java/com/wehaul/fleet/Application.java
@@ -1,7 +1,10 @@
 package com.wehaul.fleet;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +12,10 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+    @Bean
+    public HttpTraceRepository htttpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    };
 }
